cb917a445b2daf73333ca258bf667206b0a983f0
alberto <alberto.r@opendeusto.es>
2023-10-03 17:38:24 +0200

    VentanaInicial

---
+++ PixelCine/src/Metodos/Main.java
@@ -0,0 +1,13 @@
+package Metodos;
+
+import Ventanas.VentanaInicial;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub	
+		VentanaInicial vi=new VentanaInicial();
+		vi.setVisible(true);
+	}
+
+}
