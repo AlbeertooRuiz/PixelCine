285c9cf76255ac529f9cb6c57ae2bf872c0fd764
Urko <Urko@DESKTOP-LN60UOC>
2023-11-01 12:10:05 +0100

    fw

---
+++ PixelCine/src/Ventanas/ventanaCalendario.java
@@ -0,0 +1,30 @@
+package Ventanas;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+import com.toedter.calendar.JCalendar;
+
+public class ventanaCalendario extends JFrame {
+    public ventanaCalendario() {
+        setTitle("Ventana con JCalendar");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 300);
+        setLocationRelativeTo(null);
+
+        // Crear un JCalendar
+        JCalendar jCalendar = new JCalendar();
+        jCalendar.setTodayButtonVisible(true);
+
+        // Agregar el JCalendar a la ventana
+        add(jCalendar);
+
+        setVisible(true);
+        
+        setResizable(false);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new ventanaCalendario());
+    }
+}
