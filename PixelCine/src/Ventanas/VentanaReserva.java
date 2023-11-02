b758c5d767d9f6aa2faa04d2eb1fab384946fbab
Urko <Urko@DESKTOP-LN60UOC>
2023-11-02 12:49:22 +0100

    f

--- PixelCine/src/Ventanas/VentanaReserva.java
+++ PixelCine/src/Ventanas/VentanaReserva.java
@@ -108,8 +108,8 @@ public class VentanaReserva extends JFrame {
 		panelCentro.add(lblNewLabel_5);
 	}
 	
-	  public static void main(String[] args) {
-	        SwingUtilities.invokeLater(() -> new VentanaReserva());
-	    }
+	public static void main(String[] args) {
+	    SwingUtilities.invokeLater(() -> new VentanaReserva());
+	}
 
 }
