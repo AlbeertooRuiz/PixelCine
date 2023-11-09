9db54e58170720722db38146fa52dc5322dced95
aner lekue <anerlekue@opendeusto.es>
2023-11-09 13:27:32 +0100

    AsientosCOntrol

--- PixelCine/src/Ventanas/VentanaAsientos.java
+++ PixelCine/src/Ventanas/VentanaAsientos.java
@@ -2,16 +2,26 @@ package Ventanas;
 
 import javax.swing.*;
 
+import Datos.Coordenadas;
 import Datos.Pelicula;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class VentanaAsientos extends JFrame {
-    private JButton[][] celdas;
-
-    public VentanaAsientos(JFrame va, Pelicula p) {
+    private JButton[][] celdas; 
+    
+	
+    public VentanaAsientos(JFrame va, Pelicula p) {  
+    	ArrayList <Coordenadas> listaCoordenadas = new ArrayList<>();
+    	
+    	listaCoordenadas.add(new Coordenadas(3,3));
+    	listaCoordenadas.add(new Coordenadas(1,1));
+    	listaCoordenadas.add(new Coordenadas(5,5));
+    	
+  	
         // Configurar la ventana
         setTitle("Tabla de Asientos");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -57,17 +67,31 @@ public class VentanaAsientos extends JFrame {
                 // Agregar un ActionListener para cambiar el color al hacer clic
                 celdas[i][j].addActionListener(new ActionListener() {
                     @Override
-                    public void actionPerformed(ActionEvent e) {
-                        celdas[row][col].setBackground(Color.BLUE);
+                    public void actionPerformed(ActionEvent e) {                      
+                        if (celdas[row][col].getBackground() == Color.BLUE) {               	
+                        	celdas[row][col].setBackground(Color.WHITE);
+                        }else if(celdas[row][col].getBackground() == Color.WHITE){
+                  
+                        	celdas[row][col].setBackground(Color.BLUE);
+                        }
                     }
                 });
 
                 // Agregar la celda a la ventana
                 add(celdas[i][j]);
             }
+            
         }
-
+    	for (int i = 0; i < listaCoordenadas.size(); i++) {   		
+    		Coordenadas a  = listaCoordenadas.get(i);
+    		celdas[a.getX()][a.getY()].setBackground(Color.RED);
+   		 
+   	  	}
+        
         // Mostrar la ventana
         setVisible(true);
     }
+    public static void main(String[] args) {
+	    SwingUtilities.invokeLater(() -> new VentanaAsientos(null, null));
+	}
 }
