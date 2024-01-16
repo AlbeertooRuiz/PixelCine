23c1206c91083e7c7696cb5226238d09936dc4bb
aner lekue <33292278+anerlekue@users.noreply.github.com>
2024-01-16 19:27:22 +0100

    Ventana Admin Visual falta Funcionalidad

--- PixelCine/src/Ventanas/VentanaAdministrador.java
+++ PixelCine/src/Ventanas/VentanaAdministrador.java
@@ -5,6 +5,11 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.Font;
+import net.miginfocom.swing.MigLayout;
 
 public class VentanaAdministrador extends JFrame {
 
@@ -21,6 +26,29 @@ public class VentanaAdministrador extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.NORTH);
+		
+		JLabel lblTitulo = new JLabel("Ventana Administrador");
+		lblTitulo.setFont(new Font("Arial", Font.BOLD, 30));
+		panel.add(lblTitulo);
+		
+		JPanel panel_1 = new JPanel();
+		contentPane.add(panel_1, BorderLayout.SOUTH);
+		
+		JButton btnSalir = new JButton("Salir");
+		panel_1.add(btnSalir);
+		
+		JPanel panel_2 = new JPanel();
+		contentPane.add(panel_2, BorderLayout.CENTER);
+		panel_2.setLayout(new MigLayout("", "[]", "[]"));
 	}
 
+	
+	//añadir/quitar peliculas a la bd
+
+	//cambiar nombre/contraseña/email del usuario 
+	
 }
